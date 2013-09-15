674c71ad6cd51cd6b8f5f6bff64c93ecf4c1bc4d
Kane Dou <douqilong@gmail.com>
2013-09-16 04:21:31 +0800

    calendar day and week popover

---
+++ js/app/scheduling/day.js
@@ -0,0 +1,28 @@
++function ($, NAS) { 'use strict';
+  $(document).ready(function () {
+    NAS.scheduling.bindNewEvent();
+    var $calTbl = $('table.calendar.days');
+    $calTbl.on('click', '.event', function (e) {
+      var $target = $(e.currentTarget),
+          nurses = $target.html(),
+          handler = NAS.scheduling.evtDetailHandlerFactory.create({
+            detailTemplate: Handlebars.compile(
+              $('#event-detail-template').html())({
+                edit: true,
+                nurses: nurses
+              }),
+            detailElem: '.schedule-detail',
+            confirmHandler: function (e) {
+              var $trigger = $(e.data.trigger),
+                  $detail = $(e.data.detail);
+              e.preventDefault();
+              if (nurses = $.trim($detail.find('input[name=nurses]').val())) {
+                $trigger.html(nurses);
+              }
+              NAS.scheduling.closePopover($trigger);
+            }
+          });
+      handler(e);
+    });
+  });
+}(window.jQuery, window.NAS);
