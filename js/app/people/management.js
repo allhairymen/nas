837ec6684f7571e0eaf3622f09a8f206bcd66a90
Kane Dou <douqilong@gmail.com>
2013-09-20 01:16:30 +0800

    people management

--- js/app/people/management.js
+++ js/app/people/management.js
@@ -3,13 +3,13 @@
       NAS = this.NAS;
 
   $(document).ready(function ($) {
-    var $hosts = $('.hosts-management'),
+    var $people = $('.people-management'),
         $opNav = $('ul.operation-nav'),
         $editModal = $('#myModal_2'),
         $deleteModal = $('#myModal_1'),
-        hostTemplate = Handlebars.compile($('#host-template').html());
+        personTemplate = Handlebars.compile($('#person-template').html());
 
-    $hosts.on('click', 'li.new', function (e) {
+    $people.on('click', 'li.new', function (e) {
       e.preventDefault();
       var $target = $(this);
       $editModal.one('click', 'button.js-confirm-btn', function (e) {
@@ -17,7 +17,7 @@
             name = $input.val();
         if (name) {
           $editModal.one('hidden.bs.modal', function (e) {
-            $target.before(hostTemplate({name: name}));
+            $target.before(personTemplate({name: name}));
           });
           $input.val('');
           $editModal.modal('hide');
@@ -28,7 +28,7 @@
 
     $opNav.on('click', 'a.js-edit-btn', function (e) {
       e.preventDefault();
-      var $toEditHosts = $hosts.find(':checkbox:checked').parent('li'),
+      var $toEditHosts = $people.find(':checkbox:checked').parent('li'),
           quant = $toEditHosts.length;
       if (quant == 1) {
         var $input = $editModal.find('input[name=name]'),
@@ -45,26 +45,26 @@
         $editModal.modal('show');
       } else {
         if ($toEditHosts.length > 1) {
-          alert('只能对单个主机进行操作');
+          alert('只能对单个人员进行操作');
         } else if ($toEditHosts.length < 1) {
-          alert('请选择主机');
+          alert('请选择人员');
         }
       }
     });
 
     $opNav.on('click', 'a.js-delete-btn', function (e) {
       e.preventDefault();
-      var $toDeleteHosts = $hosts.find(':checkbox:checked').parent('li');
-      if ($toDeleteHosts.length < 1) return alert('请选择主机');
+      var $toDeletePeople = $people.find(':checkbox:checked').parent('li');
+      if ($toDeletePeople.length < 1) return alert('请选择人员');
       $deleteModal.one('click', '.js-confirm-btn', function (e) {
         $deleteModal.one('hidden.bs.modal', function (e) {
-          $toDeleteHosts.remove();
+          $toDeletePeople.remove();
         });
         $deleteModal.modal('hide');
       });
       $deleteModal.modal('show');
     });
 
-    $hosts.on('change', ':checkbox', NAS.facilities.onFacilityChecked);
+    $people.on('change', ':checkbox', NAS.facilities.onFacilityChecked);
   });
 }.call(this, window.jQuery);
